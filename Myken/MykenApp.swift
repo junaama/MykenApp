60114cb0ff22605e641a252f1ad4dbe595664e4a
naama <60440166+junaama@users.noreply.github.com>
2024-07-15 14:14:07 -0400

    Initial Commit

---
+++ Myken/MykenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MykenApp.swift
+//  Myken
+//
+//  Created by NAAMA on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct MykenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
